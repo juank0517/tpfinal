3ab8f3c0a2f43e8ffd1fb1ff6de0b560dad6cf91
Juan Carlos Frutos <juank0517@gmail.com>
2023-07-07 00:47:27 -0300

    Update Resumen.jsx

--- src/assets/componentes/Resumen/Resumen.jsx
+++ src/assets/componentes/Resumen/Resumen.jsx
@@ -14,7 +14,7 @@ const Resumen = () => {
                         <h2>Administrativo</h2>
                         <h4>Sanatorio Adventista del Plata, 2013</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, qui minus. Laboriosam temporibus, dolore officia labore eum quia commodi earum laudantium consectetur minima laborum placeat. Temporibus laborum quam soluta odit?
+                            Trabajé durante cinco años en el turno noche, donde desarrolle tareas administrativas como codificar estudios en el sector de la emergencia, cobros y gestión de historía clínica.
                         </p>
                     </div>
                     <div className="linea"></div>
@@ -22,7 +22,7 @@ const Resumen = () => {
                         <h2>Programador Web Full Stack</h2>
                         <h4>Sanatorio Adventista del Plata, 2018</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, qui minus. Laboriosam temporibus, dolore officia labore eum quia commodi earum laudantium consectetur minima laborum placeat. Temporibus laborum quam soluta odit?
+                            Hace cinco años que llevo trabajando en el área de programación, me dedico especialmente al desarrollo de aplicaciones Web, tanto el back end como el front end. 
                         </p>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ const Resumen = () => {
                         <h2>ASP.NET</h2>
                         <h4>UTN, 2022</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, qui minus. Laboriosam temporibus, dolore officia labore eum quia commodi earum laudantium consectetur minima laborum placeat. Temporibus laborum quam soluta odit?
+                            Aprendí a crear aplicaciones web con C# desde cero, entendiendo desde los conceptos básicos hasta los mas avanzados, usando Entity FrameWork para conectarnos y realizar operaciones con una base de datos.
                         </p>
                     </div>
                     <div className="linea"></div>
@@ -42,7 +42,7 @@ const Resumen = () => {
                         <h2>Professional Front End Developer</h2>
                         <h4>UTN, 2023</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, qui minus. Laboriosam temporibus, dolore officia labore eum quia commodi earum laudantium consectetur minima laborum placeat. Temporibus laborum quam soluta odit?
+                            Conocimiento de React hasta un nivel competitivo en el ambiente laboral de hoy en día. Este curso está construido 100% en Hooks y functional components.
                         </p>
                     </div>
                 </div>

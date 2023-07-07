5ccbf3f6d1cad49e87bacdef4bf484306479b62e
Juan Carlos Frutos <juank0517@gmail.com>
2023-07-07 01:07:25 -0300

    Update Servicios.jsx

--- src/assets/componentes/Servicios/Servicios.jsx
+++ src/assets/componentes/Servicios/Servicios.jsx
@@ -9,22 +9,22 @@ const Servicios = () => {
                 <div class="conozco">
                     <i class="bi bi-display"></i>
                     <h3>Diseño Web</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dignissimos maxime ab iusto voluptas neque, quae consequuntur rerum unde vero officiis eius fugit possimus autem nobis nostrum. Impedit, tenetur inventore?</p>
+                    <p>Desarrollo de interfaces digitales, como el diseño de sitios y aplicaciones para web utilizando lenguajes de marcado como HTML</p>
                 </div>
                 <div class="conozco">
                     <i class="bi bi-tablet"></i>
                     <h3>Desarrollo App</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dignissimos maxime ab iusto voluptas neque, quae consequuntur rerum unde vero officiis eius fugit possimus autem nobis nostrum. Impedit, tenetur inventore?</p>
+                    <p>Creación de apps de manera rápida y sencilla utilizando la plataforma React Native, creando aplicaciones móviles de todo tipo desde cero y personalizables.</p>
                 </div>
                 <div class="conozco">
                     <i class="bi bi-display"></i>
                     <h3>Metodologías Agiles</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dignissimos maxime ab iusto voluptas neque, quae consequuntur rerum unde vero officiis eius fugit possimus autem nobis nostrum. Impedit, tenetur inventore?</p>
+                    <p>Preparado para utilizar estrategias que aumente la competitividad que exige el mercado actual, implementación de metodologías que impliquen optimización y rendimiento</p>
                 </div>
                 <div class="conozco">
                     <i class="bi bi-brush"></i>
                     <h3>Diseño UX/UI</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dignissimos maxime ab iusto voluptas neque, quae consequuntur rerum unde vero officiis eius fugit possimus autem nobis nostrum. Impedit, tenetur inventore?</p>
+                    <p>El diseño de experiencias no es un proceso exacto y eso es lo que permite que cada solución sea completamente diferente, en juego factores como la creatividad y la resiliencia.</p>
                 </div>
                 <div class="conozco">
                     <i class="bi bi-display"></i>

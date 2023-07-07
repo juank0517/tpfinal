4182941347514a647e834b7232d079bfa752dfcb
Juan Carlos Frutos <juank0517@gmail.com>
2023-07-07 01:26:58 -0300

    Update Footer.jsx

--- src/assets/componentes/Footer/Footer.jsx
+++ src/assets/componentes/Footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
         <footer>
             <img src="images/logo.svg" alt="" />
             <div class="redes">
-                <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.facebook.com/juank0517" target="_blank"><i class="bi bi-facebook"></i></a>
                 <a href="#"><i class="bi bi-twitter"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/juan-carlos-frutos-a4894313b/" target="_blank"><i class="bi bi-linkedin"></i></a>
+                <a href="https://www.instagram.com/juankfrutos/" target="_blank"><i class="bi bi-instagram"></i></a>
             </div>
             <label>Juan Carlos Frutos @ 2023 Todos los derechos reservados</label>
         </footer>
